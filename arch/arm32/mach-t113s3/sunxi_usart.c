7b194c79dd223ed2eb0bbd5aadc280a20d106032
Fabien Poussin <fabien.poussin@gmail.com>
2023-01-23 20:15:22 +0100

    sunxi_usart: add read (get) and baudrate parameter

--- arch/arm32/mach-t113s3/sunxi_usart.c
+++ arch/arm32/mach-t113s3/sunxi_usart.c
@@ -4,10 +4,28 @@
 #include "sunxi_usart.h"
 #include "reg-ccu.h"
 
-void sunxi_usart_init(const sunxi_usart_t *usart)
+// Macros to access UART registers.
+#define UART_RBR(n) *(volatile uint32_t *)(UART_BASE(n) + 0x00)
+#define UART_THR(n) *(volatile uint32_t *)(UART_BASE(n) + 0x00)
+#define UART_DLL(n) *(volatile uint32_t *)(UART_BASE(n) + 0x00)
+#define UART_IER(n) *(volatile uint32_t *)(UART_BASE(n) + 0x04)
+#define UART_FCR(n) *(volatile uint32_t *)(UART_BASE(n) + 0x08)
+#define UART_LCR(n) *(volatile uint32_t *)(UART_BASE(n) + 0x0C)
+#define UART_MCR(n) *(volatile uint32_t *)(UART_BASE(n) + 0x10)
+#define UART_LSR(n) *(volatile uint32_t *)(UART_BASE(n) + 0x14)
+#define UART_USR(n) *(volatile uint32_t *)(UART_BASE(n) + 0x7C)
+
+#define UART_USR_BUSY (0x1 << 0)
+#define UART_USR_TFNF (0x1 << 1)
+#define UART_USR_TFE  (0x1 << 2)
+#define UART_USR_RFNE (0x1 << 3)
+#define UART_USR_RFF  (0x1 << 4)
+
+void sunxi_usart_init(const sunxi_usart_t *usart, uint32_t baudrate)
 {
 	uint32_t addr;
 	uint32_t val;
+	uint32_t div = 13;
 
 	/* Config usart TXD and RXD pins */
 	sunxi_gpio_init(usart->gpio_tx.pin, usart->gpio_tx.mux);
@@ -25,43 +43,25 @@ void sunxi_usart_init(const sunxi_usart_t *usart)
 	val |= 1 << (16 + usart->id);
 	write32(addr, val);
 
-	/* Config USART to 115200-8-1-0 */
-	addr = usart->base;
-	write32(addr + 0x04, 0x0);
-	write32(addr + 0x08, 0xf7);
-	write32(addr + 0x10, 0x0);
-	val = read32(addr + 0x0c);
-	val |= (1 << 7);
-	write32(addr + 0x0c, val);
-	write32(addr + 0x00, 0xd & 0xff);
-	write32(addr + 0x04, (0xd >> 8) & 0xff);
-	val = read32(addr + 0x0c);
-	val &= ~(1 << 7);
-	write32(addr + 0x0c, val);
-	val = read32(addr + 0x0c);
-	val &= ~0x1f;
-	val |= (0x3 << 0) | (0 << 2) | (0x0 << 3);
-	write32(addr + 0x0c, val);
+	div = 1500000 / baudrate;
+
+	// Configure baud rate
+	UART_LCR(usart->id) = (1 << 7) | 3;
+	UART_DLL(usart->id) = div;
+	UART_LCR(usart->id) = 3;
+
+	// Enable FIFO
+	UART_FCR(usart->id) = 0x00000001;
 }
-
-#define UART_RBR 0x00
-#define UART_THR 0x00
-#define UART_USR 0x7c
-
-#define UART_USR_BUSY (0x1 << 0)
-#define UART_USR_TFNF (0x1 << 1)
-#define UART_USR_TFE  (0x1 << 2)
-#define UART_USR_RFNE (0x1 << 3)
-#define UART_USR_RFF  (0x1 << 4)
 
 void sunxi_usart_putc(void *arg, char c)
 {
 	sunxi_usart_t *usart = (sunxi_usart_t *)arg;
 
-	while ((read32(usart->base + UART_USR) & UART_USR_TFNF) == 0)
+	while ((UART_USR(usart->id) & UART_USR_TFNF) == 0)
 		;
-	write32(usart->base + UART_THR, c);
-	while ((read32(usart->base + UART_USR) & UART_USR_BUSY) == 1)
+	UART_THR(usart->id) = c;
+	while ((UART_USR(usart->id) & UART_USR_BUSY) == 1)
 		;
 }
 
@@ -76,8 +76,8 @@ void sunxi_usart_put(sunxi_usart_t *usart, char *in, uint32_t len)
 void sunxi_usart_get(sunxi_usart_t *usart, char *out, uint32_t len)
 {
 	while (len--) {
-		while ((read32(usart->base + UART_USR) & UART_USR_RFNE) == 0) {
+		while ((UART_USR(usart->id) & UART_USR_RFNE) == 0) {
 		};
-		*out++ = read32(usart->base + UART_RBR) & 0xff;
+		*out++ = UART_RBR(usart->id) & 0xff;
 	}
 }
--- arch/arm32/mach-t113s3/sunxi_usart.h
+++ arch/arm32/mach-t113s3/sunxi_usart.h
@@ -4,14 +4,16 @@
 #include "main.h"
 #include "sunxi_gpio.h"
 
+// The UART registers base address.
+#define UART_BASE(id) ((uint32_t)(0x02500000 + (0x400 * id)))
+
 typedef struct {
-	uint32_t   base;
 	uint8_t	   id;
 	gpio_mux_t gpio_tx;
 	gpio_mux_t gpio_rx;
 } sunxi_usart_t;
 
-void sunxi_usart_init(const sunxi_usart_t *usart);
+void sunxi_usart_init(const sunxi_usart_t *usart, uint32_t baudrate);
 void sunxi_usart_putc(void *arg, char c);
 void sunxi_usart_put(sunxi_usart_t *usart, char *in, uint32_t len);
 void sunxi_usart_get(sunxi_usart_t *usart, char *out, uint32_t len);
